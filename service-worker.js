0bcfd8db8528068b1fc03de9bfe159ac0e8394fa
machadocc <104290845+machadocc@users.noreply.github.com>
2025-09-04 19:50:32 -0300

    Primeiro commit - subida do projeto Catálogo Responsivo

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', event => {
   console.log('Service Worker instalado');
   self.skipWaiting(); 
-});
+}); 
 
 self.addEventListener('activate', event => {
   console.log('Service Worker ativado');
